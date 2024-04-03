ceebaae5d866945490bc712182261e75f292ced4
Filipe <130001537+Filipe-16@users.noreply.github.com>
2024-04-03 08:33:17 -0300

    delete.php

--- delete.php
+++ delete.php
@@ -3,7 +3,7 @@
 echo "Remover";
 
 $id_remover = $_POST['id'];
-$conexao =  mysqli_connect("127.0.0.1", "root", "", "crud_musicas");
+$conexao = mysqli_connect("localhost", "root", "mysqluser", "crud_musicas");
 $query = "DELETE FROM musicas WHERE id=".$id_remover;
 
 echo "$query";
