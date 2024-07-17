e75e544dac726f8e82f8949eef43f4ed752aa48f
Filipe <130001537+Filipe-16@users.noreply.github.com>
2024-07-17 09:44:47 -0300

    delete.php

--- delete.php
+++ delete.php
@@ -1,13 +1,19 @@
 <?php
-
+// Exibe a mensagem
 echo "Remover";
 
+// Obtém o ID das músicas que vão ser removidas via POST
 $id_remover = $_POST['id'];
+
+// Conecta o Banco de Dados
 $conexao = mysqli_connect("localhost", "root", "mysqluser", "crud_musicas");
+
+// Monta a query SQL para deletar a música utilizando o ID fornecido
 $query = "DELETE FROM musicas WHERE id=".$id_remover;
 
 echo "$query";
 
+// Executa a query para deletar a música
 $resultado = mysqli_query($conexao, $query);
 
 ?>
