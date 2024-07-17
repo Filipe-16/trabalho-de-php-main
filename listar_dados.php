0ccc2dbcf659848b407ac4cc222b8d1348f4cdbb
Filipe <130001537+Filipe-16@users.noreply.github.com>
2024-07-17 09:46:07 -0300

    listar_dados.php

--- listar_dados.php
+++ listar_dados.php
@@ -1,24 +1,33 @@
 <?php
 
+// Exibe a mensagem
 echo "Banco De Dados";
 
+// Conexão o Banco de Dados
 $conexao = mysqli_connect("localhost", "root", "mysqluser", "crud_musicas");
 
+// Atualiza os dados do filme caso o formulário seja submetido
 if (!empty($_POST['id_for_updating'])){
     $nome_edit = $_POST['nome_edit'];
     $artista_edit = $_POST['artista_edit'];
     $ano_edit = $_POST['ano_edit'];
     $id_for_updating = $_POST['id_for_updating']; 
 
+    // Query para atualizar os dados da música 
     $query = "UPDATE musicas SET nome='$nome_edit', artista='$desenvolvedora_edit', ano='$genero_edit' WHERE id=$id_for_updating";
 
+    // Executa a query para atualização
     mysqli_query($conexao, $query);
 }
 
+// Remove uma música se o formulário de remoção foi submetido
 if (!empty($_POST["id_for_removing"])){
-    $removingRow = $_POST["id_for_removing"];   
+    $removingRow = $_POST["id_for_removing"]; 
+    
+    // Query para remover a música do banco de dados
     $query_for_removing = "DELETE FROM musicas WHERE id=$removingRow";
 
+    // Executa a query para remover música
     mysqli_query($conexao, $query_for_removing);
 
 }
