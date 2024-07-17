c4617d38b7aaf54f7fca820eb1d0fe20edc18c8a
Filipe <130001537+Filipe-16@users.noreply.github.com>
2024-07-17 09:45:02 -0300

    editar_data.php

--- editar_data.php
+++ editar_data.php
@@ -1,32 +1,38 @@
 <?php
 
+// Obtém os dados do formulário via POST
 $nome = $_POST["nome_edit"];
 $artista = $_POST["artista_edit"];
 $ano = $_POST["ano_edit"];
 $id_atualizacao = $_POST["id_atualizacao"];
 $diretorio = "uploads/";
 
+// Verifica se o formulário foi submetido
 if(isset($_POST["submit"])) { 
+    // Conecta o Banco de Dados
+    $conexao = mysqli_connect("localhost", "root", "mysqluser", "crud_musicas");
 
-    $conexao = mysqli_connect("127.0.0.1","root","","crud_filmes");
-
+    // Verifica se um arquivo de imagem foi enviado
     if(!empty($_FILES["imagem_edit"]["name"])) { 
         $nome_arquivo = basename($_FILES["imagem_edit"]["name"]);
     
         $nome_arquivo_modificado = date("YmdHis").$nome_arquivo;
-    
+
+        // Define os caminhos do arquivo que vai ser enviado
         $caminho_arquivo = $diretorio . $nome_arquivo ; 
 
         $tipo_arquivo = pathinfo($caminho_arquivo, PATHINFO_EXTENSION);
         
-        $caminho_arquivo_alvo = $diretorio.$nome_arquivo_modificado; 
+        $caminho_arquivo_alvo = $diretorio.$nome_arquivo_modificado;
 
+        // Tipos de arquivo que são permitidos
         $tipos_permitidos = array('jpg','png','jpeg','gif'); 
 
+        // Verifica se o tipo do arquivo é permitido
         if(in_array($tipo_arquivo, $tipos_permitidos)) { 
             if(move_uploaded_file($_FILES["imagem_edit"]["tmp_name"], $caminho_arquivo_alvo)) { 
               
-                $query = "UPDATE filmes SET nome='$nome', artista='$artista', ano='$ano', imagem ='$nome_arquivo_modificado' WHERE id=$id_atualizacao";
+                $query = "UPDATE musicas SET nome='$nome', artista='$artista', ano='$ano', imagem ='$nome_arquivo_modificado' WHERE id=$id_atualizacao";
                 $inserir = mysqli_query($conexao,$query); 
 
                 if($inserir) { 
@@ -45,7 +51,7 @@ if(isset($_POST["submit"])) {
         } 
     } 
     else { 
-        $query = "UPDATE filmes SET nome='$nome', artista='$artista', ano='$ano' WHERE id=$id_atualizacao";
+        $query = "UPDATE musicas SET nome='$nome', artista='$artista', ano='$ano' WHERE id=$id_atualizacao";
         $inserir = mysqli_query($conexao, $query); 
 
         if($inserir) { 
@@ -57,6 +63,7 @@ if(isset($_POST["submit"])) {
     }
 }
 
+// Exibe a mensagem de resultado
 echo $mensagem;
 
 ?>
